aee60a9940b5e54dabf2dec4fe851e42d5a5d3cb
aespears <80167534+aespears@users.noreply.github.com>
2022-11-08 16:59:15 -0500

    Testing with examples

--- correctTests_testing.R
+++ correctTests_testing.R
@@ -1,19 +1,25 @@
 library(R.utils)
-# Very simple, incomplete version.
-correctTests <- function(x, s){
-  # need to translate this into a numeric score
-  # But I think saving the comments somehow is helpful?
-  ae <- (all.equal(x, s))
-  return(list(score = as.numeric(isTRUE(ae)), comments = ae))
-  
-}
+source('correctness.R')
 
-# Thought: If correctCheck_object passes all the timeout/error requirements, 
-# then have it return the results from a new function (correctTests), that way 
-# we can save the all.equal comments 
-
-t <- correctCheck_directory('662/feedback/ExampleSubmissions-1/', 
+# Example 1: Return a variable ans
+t1 <- correctCheck_directory('662/feedback/ExampleSubmissions-1/', 
                             '662/feedback/Example1_Solution1.R', timeout = 3)
 
-feedbacks <- lapply(t, function(x) return(x['correctCheck']))
+scores <- lapply(t1, function(x) return(x['score']))
+
+feedbacks <- lapply(t1, function(x) return(x['comments']))
 table(unlist(feedbacks))
+table(unlist(scores))
+
+######################################################################
+# Example 2: Write a function, use test cases
+
+# Toy dataframe for testing
+df <- data.frame(letters = c('A', 'B', NA, 'X', NA),
+                 numbers = c(NA, 31, 14, NA, 89))
+
+t2 <- correctCheck_directory('662/example2_1', 
+                            '662/example2_1/spearsabby_168704_6630827_Question2.R', 
+                            timeout = 3, iris, df)
+
+
